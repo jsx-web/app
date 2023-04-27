7c110e8419d68e2b204eecfd88ae0a99a6c40546
JSX <1873802229@qq.com>
2023-04-27 16:11:24 +0800

    4_27_1

--- iec104/slavemain.cpp
+++ iec104/slavemain.cpp
@@ -31,7 +31,7 @@ int main(int argc,char * argv[])
         pthread_t tid1;
         pthread_t tidw;
         pthread_create(&tid1,NULL,k_session,(void *)&connfd);
-        //pthread_create(&tidw,NULL,k_session,(void *)&connfd);//双线程抢占共享资源了。。。。。
+        pthread_create(&tidw,NULL,Report_data,(void *)&connfd);//实时发送数据                                      //bug1双线程抢占共享资源了。。。。。
     }
     pthread_exit(NULL);
     close(sockfd);
